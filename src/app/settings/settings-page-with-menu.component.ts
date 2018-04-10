0c28a66ed81f914767609f14ec31589e68e94bef
developingstar <petr.nocolae812@gmail.com>
2018-04-10 18:04:23 +0200

    Build dynamic left menu of settings

--- src/app/settings/settings-page-with-menu.component.ts
+++ src/app/settings/settings-page-with-menu.component.ts
@@ -1,10 +1,15 @@
-import { Component, Input } from '@angular/core'
-import { NavController } from 'ionic-angular'
+import { Component, Input, OnDestroy } from '@angular/core'
+import { NavController, NavParams } from 'ionic-angular'
+import { Observable, Subscription } from 'rxjs'
+
+import { IntegrationsService } from './integrations.service'
+import { Service } from './service.model'
 
 interface IMenuEntry {
   readonly children?: ReadonlyArray<IMenuEntry>
   readonly label: string
   readonly link?: string
+  readonly id?: number
 }
 
 @Component({
@@ -14,39 +19,74 @@ interface IMenuEntry {
 export class SettingsPageWithMenuComponent {
   @Input() readonly currentPage: string
 
-  readonly menu: ReadonlyArray<IMenuEntry> = [
-    { label: 'Account Settings', link: 'AccountSettingsPage' },
-    { label: 'Billing Settings' },
-    {
-      children: [{ label: 'Team members' }, { label: 'Sales groups' }],
-      label: 'Team Settings'
-    },
-    {
-      children: [{ label: 'Twilio' }, { label: 'Zapier' }],
-      label: 'Integrations'
-    },
-    {
-      children: [
-        { label: 'Pipelines', link: 'PipelinesPage' },
-        { label: 'Custom Fields', link: 'CustomFieldsPage' }
-      ],
-      label: 'CMS Settings'
-    }
-  ]
+  private readonly integrationsChildren: Observable<ReadonlyArray<IMenuEntry>>
+  private readonly integrationsSubscription: Subscription
 
-  constructor(private readonly nav: NavController) {}
+  constructor(
+    private navParams: NavParams,
+    private readonly nav: NavController,
+    private readonly integrationsService: IntegrationsService
+  ) {
+    this.integrationsChildren = this.integrationsService
+      .services()
+      .map((services: ReadonlyArray<Service>) => {
+        return services.map((service) => ({
+          id: service.id,
+          label: service.name,
+          link: 'IntegrationPage'
+        }))
+      })
+      .shareReplay(1)
+    this.integrationsSubscription = this.integrationsChildren.subscribe()
+  }
 
-  goTo(page: string | undefined): void {
-    if (page) {
-      this.nav.setRoot(page)
+  ngOnDestroy(): void {
+    if (this.integrationsSubscription) {
+      this.integrationsSubscription.unsubscribe()
     }
   }
 
-  isActive(entry: IMenuEntry): boolean {
+  goTo(page: string | undefined, id: number | undefined): void {
+    if (page) {
+      if (id) {
+        this.nav.setRoot(page, { id: id })
+      } else {
+        this.nav.setRoot(page)
+      }
+    }
+  }
+
+  isActive(entry: IMenuEntry, id: number | undefined): boolean {
+    const serviceID = Number(this.navParams.get('id'))
     return (
-      this.currentPage === entry.link ||
+      (this.currentPage === entry.link &&
+        (id === undefined || id === serviceID)) ||
       (entry.children !== undefined &&
-        entry.children.some((child) => this.isActive(child)))
+        entry.children.some((child) => this.isActive(child, child.id)))
     )
+  }
+
+  get menu(): Observable<ReadonlyArray<IMenuEntry>> {
+    return this.integrationsChildren.map((integrationsChildren) => [
+      { label: 'Account Settings', link: 'AccountSettingsPage' },
+      { label: 'Billing Settings' },
+      {
+        children: [{ label: 'Team members' }, { label: 'Sales groups' }],
+        label: 'Team Settings'
+      },
+      {
+        children: integrationsChildren,
+        id: integrationsChildren[0].id,
+        label: 'Integrations',
+        link: 'IntegrationPage'
+      },
+      {
+        children: [
+          { label: 'Pipelines', link: 'PipelinesPage' },
+          { label: 'Custom Fields', link: 'CustomFieldsPage' }
+        ],
+        label: 'CMS Settings'
+      }
+    ])
   }
 }
