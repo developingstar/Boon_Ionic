c455acc3615ac8c56e3b9c6cec4858ff265f3cc1
developingstar <petr.nocolae812@gmail.com>
2018-05-16 10:48:49 +0200

    Add toast to Integration page

--- src/app/settings/integration.page.ts
+++ src/app/settings/integration.page.ts
@@ -1,8 +1,9 @@
 import { Component } from '@angular/core'
-import { AlertController, IonicPage, NavParams } from 'ionic-angular'
+import { IonicPage, NavParams, ToastController } from 'ionic-angular'
 import { Observable } from 'rxjs'
 
 import { ReactivePage } from '../utils/reactive-page'
+import { toastSuccessDefaults } from '../utils/toast'
 import { initialState, IState, UserAction } from './integration.page.state'
 import { IntegrationsService } from './integrations.service'
 import { Service } from './service.model'
@@ -15,55 +16,16 @@ import { Service } from './service.model'
   templateUrl: 'integration.page.html'
 })
 export class IntegrationPage extends ReactivePage<IState, UserAction> {
-  navSubscribe: any
-  originalService: Service
-  isChanged: boolean
-
   constructor(
     public navParams: NavParams,
-    public alertCtrl: AlertController,
+    private readonly toastController: ToastController,
     private readonly integrationsService: IntegrationsService
   ) {
     super(initialState)
-    this.isChanged = false
-  }
-
-  ionViewCanLeave(): Promise<boolean> {
-    return new Promise((resolve, reject) => {
-      const alert = this.alertCtrl.create({
-        buttons: [
-          {
-            handler: () => {
-              this.uiActions.next({ name: 'update_service' })
-              resolve(true)
-            },
-            text: 'Save'
-          },
-          {
-            handler: () => {
-              resolve(true)
-            },
-            text: "Don't Save"
-          }
-        ],
-        subTitle:
-          'You have chaged somethingsomething. Do you want to save them?',
-        title: 'Confirm'
-      })
-      if (!this.isChanged) resolve(true)
-      else alert.present()
-    })
   }
 
   public updateService(): void {
     this.uiActions.next({ name: 'update_service' })
-  }
-
-  public tokenChanged(): void {
-    this.service.subscribe((service: Service) => {
-      this.isChanged =
-        service.token !== this.originalService.token ? true : false
-    })
   }
 
   get service(): Observable<Service> {
@@ -81,31 +43,34 @@ export class IntegrationPage extends ReactivePage<IState, UserAction> {
     const serviceID = Number(this.navParams.get('id'))
     const selectedService = this.integrationsService
       .service(serviceID)
-      .map((service) => {
-        if (!this.originalService) {
-          this.originalService = new Service({
-            id: service.id,
-            name: service.name,
-            token: service.token
-          })
-        }
-        return {
-          name: 'edit',
-          service: service
-        }
-      })
+      .map((service) => ({
+        name: 'edit',
+        service: service
+      }))
     switch (action.name) {
       case 'edit':
         return selectedService
       case 'update_service':
         return this.integrationsService
           .updateService(serviceID, state.service)
-          .map((service) => ({
-            ...state,
-            service: service
-          }))
+          .map((service) => {
+            this.toast('Update token successfully.')
+            return {
+              ...state,
+              service: service
+            }
+          })
       default:
         return Observable.of(state)
     }
+  }
+
+  private toast(message: string): void {
+    this.toastController
+      .create({
+        ...toastSuccessDefaults,
+        message: message
+      })
+      .present()
   }
 }
