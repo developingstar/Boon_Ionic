f63dc727f43f69f6b1fcd3b523d9d771ec5fc6a8
developingstar <petr.nocolae812@gmail.com>
2018-05-17 11:26:17 +0200

    create alert service

---
+++ src/app/settings/alert.service.ts
@@ -0,0 +1,32 @@
+import { Injectable } from '@angular/core'
+import { AlertController } from 'ionic-angular'
+
+@Injectable()
+export class AlertService {
+  constructor(public alertCtrl: AlertController) {}
+
+  showSaveConfirmDialog(handleYes: any, handleNo: any): Promise<boolean> {
+    return new Promise((resolve, reject) => {
+      const alert = this.alertCtrl.create({
+        buttons: [
+          {
+            handler: () => {
+              resolve(handleYes())
+            },
+            text: 'Yes'
+          },
+          {
+            handler: () => {
+              resolve(handleNo())
+            },
+            text: 'No'
+          }
+        ],
+        subTitle:
+          'You have changed something. Are you sure you want to leave without saving?',
+        title: 'Confirm'
+      })
+      alert.present()
+    })
+  }
+}
