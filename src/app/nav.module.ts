2962c5160e036048a2117e2f86f36baea065a043
developingstar <petr.nocolae812@gmail.com>
2018-05-28 05:33:23 +0200

    import auto complete into nav module

--- src/app/nav.module.ts
+++ src/app/nav.module.ts
@@ -1,8 +1,10 @@
 import { PortalModule } from '@angular/cdk/portal'
 import { CommonModule } from '@angular/common'
 import { NgModule } from '@angular/core'
+import { IonicPageModule } from 'ionic-angular'
 import { InlineSVGModule } from 'ng-inline-svg'
-
+import { AutoCompleteModule } from 'primeng/autocomplete'
+import { LeadFilterService } from './nav/lead.filter.service'
 import { NavContentComponent } from './nav/nav-content.component'
 import { NavIconsComponent } from './nav/nav-icons.component'
 import { NavComponent } from './nav/nav.component'
@@ -10,6 +12,13 @@ import { NavComponent } from './nav/nav.component'
 @NgModule({
   declarations: [NavComponent, NavContentComponent, NavIconsComponent],
   exports: [NavComponent, NavContentComponent, NavIconsComponent],
-  imports: [CommonModule, InlineSVGModule, PortalModule]
+  imports: [
+    CommonModule,
+    InlineSVGModule,
+    PortalModule,
+    AutoCompleteModule,
+    IonicPageModule.forChild(NavComponent)
+  ],
+  providers: [LeadFilterService]
 })
 export class NavModule {}
