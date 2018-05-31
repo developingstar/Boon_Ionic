cf0461b397082321293dec795125f5a90be8d228
developingstar <petr.nocolae812@gmail.com>
2018-05-31 09:55:01 +0200

    implement fake api for testing

--- src/app/nav/lead.filter.service.ts
+++ src/app/nav/lead.filter.service.ts
@@ -1,29 +1,75 @@
 import { HttpClient } from '@angular/common/http'
 import { Injectable } from '@angular/core'
-import { AutoCompleteService } from 'ionic2-auto-complete'
 import { Observable } from 'rxjs'
 
 import { Lead } from '../crm/lead.model'
 @Injectable()
-export class LeadFilterService implements AutoCompleteService {
+export class LeadFilterService {
   labelAttribute = 'name'
 
   constructor(private readonly http: HttpClient) {}
 
-  public getResults(query: string): Observable<any[]> {
+  public getResults(query: string): Observable<Crm.API.ISearchDropdownItem[]> {
     return this.http
       .get('/api/leads?query=' + query)
       .map((response: Crm.API.ILeadsResponse) => {
         if (response.data) {
-          const results = response.data.leads.map((lead: Crm.API.ILead) => {
-            const leadModel = new Lead(lead)
-            let name = leadModel.name ? leadModel.name : leadModel.email
-            name = name ? name : leadModel.phoneNumber
-            return {
-              id: leadModel.id,
-              name: name
+          const results = response.data.leads.map(
+            (lead: Crm.API.ILead, index: number) => {
+              const leadModel = new Lead(lead)
+              const name = leadModel.searchDisplayName()
+              if (index === 0) {
+                return {
+                  comment: '$13, 000',
+                  group_name: 'Group1',
+                  id: leadModel.id,
+                  name: name
+                }
+              } else {
+                return {
+                  comment: '$11, 000',
+                  id: leadModel.id,
+                  name: name
+                }
+              }
             }
-          })
+          )
+          return results
+        } else {
+          Observable.throw({ message: 'Internal Server Error' })
+          return []
+        }
+      })
+  }
+
+  public getTestResults(
+    query: string = 'vin'
+  ): Observable<Crm.API.ISearchDropdownItem[]> {
+    // Fake API for testing
+    return this.http
+      .get('/api/leads?query=' + query)
+      .map((response: Crm.API.ILeadsResponse) => {
+        if (response.data) {
+          const results = response.data.leads.map(
+            (lead: Crm.API.ILead, index: number) => {
+              const leadModel = new Lead(lead)
+              const name = leadModel.searchDisplayName()
+              if (index === 0) {
+                return {
+                  comment: 'main@email.com',
+                  group_name: 'Group2',
+                  id: leadModel.id,
+                  name: name
+                }
+              } else {
+                return {
+                  comment: 'example' + leadModel.id + '@email.com',
+                  id: leadModel.id,
+                  name: name
+                }
+              }
+            }
+          )
           return results
         } else {
           Observable.throw({ message: 'Internal Server Error' })
