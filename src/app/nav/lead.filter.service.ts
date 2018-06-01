f3740e296b5881862942dcb0107fc5bf00eb102b
developingstar <petr.nocolae812@gmail.com>
2018-06-01 07:03:49 +0200

    remove fake data and api

--- src/app/nav/lead.filter.service.ts
+++ src/app/nav/lead.filter.service.ts
@@ -41,40 +41,4 @@ export class LeadFilterService {
         }
       })
   }
-
-  public getTestResults(
-    query: string = 'vin'
-  ): Observable<Crm.API.ISearchDropdownItem[]> {
-    // Fake API for testing
-    return this.http
-      .get('/api/leads?query=' + query)
-      .map((response: Crm.API.ILeadsResponse) => {
-        if (response.data) {
-          const results = response.data.leads.map(
-            (lead: Crm.API.ILead, index: number) => {
-              const leadModel = new Lead(lead)
-              const name = leadModel.searchDisplayName()
-              if (index === 0) {
-                return {
-                  comment: 'main@email.com',
-                  group_name: 'Group2',
-                  id: leadModel.id,
-                  name: name
-                }
-              } else {
-                return {
-                  comment: 'example' + leadModel.id + '@email.com',
-                  id: leadModel.id,
-                  name: name
-                }
-              }
-            }
-          )
-          return results
-        } else {
-          Observable.throw({ message: 'Internal Server Error' })
-          return []
-        }
-      })
-  }
 }
