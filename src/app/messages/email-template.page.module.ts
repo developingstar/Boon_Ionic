3916a6f1f9acfd4bcb1e6164fb1c389d6d3f3a6e
developingstar <petr.nocolae812@gmail.com>
2018-06-06 12:15:38 +0200

    create email template

--- src/app/messages/email-template.page.module.ts
+++ src/app/messages/email-template.page.module.ts
@@ -1,14 +1,17 @@
 import { NgModule } from '@angular/core'
-import { FroalaEditorModule, FroalaViewModule } from 'angular-froala-wysiwyg'
 import { IonicPageModule } from 'ionic-angular'
+import { CKEditorModule } from 'ng2-ckeditor'
 
 import { NavModule } from '../nav.module'
 import { EmailTemplatePage } from './email-template.page'
 
-import 'froala-editor/js/froala_editor.pkgd.min.js'
 @NgModule({
   declarations: [EmailTemplatePage],
   entryComponents: [EmailTemplatePage],
-  imports: [IonicPageModule.forChild(EmailTemplatePage), NavModule, FroalaEditorModule.forRoot(), FroalaViewModule.forRoot()]
+  imports: [
+    IonicPageModule.forChild(EmailTemplatePage),
+    NavModule,
+    CKEditorModule
+  ]
 })
 export class EmailTemplatePageModule {}
