99e489344ea089ac816fcb2263af54fe6678c971
developingstar <petr.nocolae812@gmail.com>
2018-06-28 09:51:17 +0200

    include pipeline component to deals show page

---
+++ src/app/crm/deals-show.page.module.ts
@@ -0,0 +1,22 @@
+import { NgModule } from '@angular/core'
+import { IonicPageModule } from 'ionic-angular'
+import { InlineSVGModule } from 'ng-inline-svg'
+
+import { PipelineComponent } from '../crm/pipeline.component'
+import { NavModule } from '../nav.module'
+import { ShowTabsComponentsModule } from '../show-tabs/show-tabs.components.module'
+import { DealsShowPage } from './deals-show.page'
+import { DealsComponentsModule } from './deals.components.module'
+@NgModule({
+  declarations: [DealsShowPage, PipelineComponent],
+  entryComponents: [DealsShowPage],
+  exports: [PipelineComponent],
+  imports: [
+    InlineSVGModule,
+    IonicPageModule.forChild(DealsShowPage),
+    NavModule,
+    DealsComponentsModule,
+    ShowTabsComponentsModule
+  ],
+})
+export class DealsShowPageModule {}
