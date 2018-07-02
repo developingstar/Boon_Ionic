e209864eb749e1a1a4d15f809bfcbd37f84887bb
developingstar <petr.nocolae812@gmail.com>
2018-07-02 05:48:24 +0200

    create unit test for deal model and service

---
+++ test/unit/deals/deals.service.test.ts
@@ -0,0 +1,83 @@
+import {
+  HttpClientTestingModule,
+  HttpTestingController
+} from '@angular/common/http/testing'
+import { async, TestBed } from '@angular/core/testing'
+
+import { HttpParams } from '@angular/common/http'
+import { IHttpRequestOptions } from '../../../src/app/api/http-request-options'
+import { Deal } from '../../../src/app/deals/deal.model'
+import { DealsService } from '../../../src/app/deals/deals.service'
+import { sampleDeal } from '../../support/factories'
+import { PaginatedList } from './../../../src/app/api/paginated-list'
+
+describe('DealsService', () => {
+  let httpMock: HttpTestingController
+  let service: DealsService
+  beforeEach(
+    async(() => {
+      TestBed.configureTestingModule({
+        imports: [HttpClientTestingModule],
+        providers: [DealsService]
+      })
+      httpMock = TestBed.get(HttpTestingController)
+      service = TestBed.get(DealsService)
+    })
+  )
+  describe('deals', () => {
+    it(
+      'returns an API collection',
+      async(() => {
+        const deal = sampleDeal()
+        service.deals().subscribe((result: PaginatedList<Deal>) => {
+          expect(result.nextPageLink).toEqual('http://example.com/next')
+          expect(result.prevPageLink).toEqual('http://example.com/prev')
+          expect(result.items[0]).toEqual(new Deal(deal))
+          expect(result.items[0] instanceof Deal).toBeTruthy()
+          expect(result.items.length).toEqual(1)
+        })
+        const req = httpMock.expectOne('/api/deals?per_page=50')
+        expect(req.request.method).toBe('GET')
+        req.flush({
+          data: {
+            deals: [JSON.parse(JSON.stringify(deal))]
+          },
+          links: {
+            next: 'http://example.com/next',
+            prev: 'http://example.com/prev'
+          },
+          metadata: {
+            count: 1
+          }
+        })
+        httpMock.verify()
+      })
+    )
+    it(
+      'allows to use a custom URL',
+      async(() => {
+        const url = 'http://example.com/api/deals?cursor=10'
+        const params: IHttpRequestOptions = {
+          params: new HttpParams(),
+          url: url
+        }
+        service.deals(params).subscribe()
+        const req = httpMock.expectOne(url)
+        expect(req.request.method).toBe('GET')
+        req.flush({
+          data: {
+            deals: []
+          },
+          links: {
+            next: null,
+            prev: null
+          },
+          metadata: {
+            count: 1
+          }
+        })
+        httpMock.verify()
+      })
+    )
+  })
+})
