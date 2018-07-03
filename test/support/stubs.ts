801cf847612d5167a8806a845a8c4e788c61b423
developingstar <petr.nocolae812@gmail.com>
2018-07-03 10:31:26 +0200

    add canGoBack function test

---
+++ test/support/stubs.ts
@@ -0,0 +1,42 @@
+import { BehaviorSubject, Observable } from 'rxjs'
+
+import { User } from '../../src/app/auth/user.model'
+import { sampleUser } from './factories'
+
+export class NavControllerStub {
+  constructor(public active: {} = { name: 'CrmPage' }) {}
+
+  getActive(): {} {
+    return this.active
+  }
+
+  setRoot(): void {
+    return
+  }
+
+  push(): void {
+    return
+  }
+
+  pop(): void {
+    return
+  }
+
+  canGoBack(): boolean {
+    return true
+  }
+}
+
+// tslint:disable-next-line:max-classes-per-file
+export class CurrentUserServiceStub {
+  private details: BehaviorSubject<User>
+  constructor(user?: User) {
+    const mergedUser = new User(sampleUser(user))
+    this.details = new BehaviorSubject(mergedUser)
+  }
+  public role(): Observable<Auth.API.Role | undefined> {
+    return this.details.map(
+      (details) => (details !== undefined ? details.role : undefined)
+    )
+  }
+}
