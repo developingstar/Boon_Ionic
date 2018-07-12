73747c7a91da2234e7b43e4444c16b1d655645ad
developingstar <petr.nocolae812@gmail.com>
2018-07-12 13:54:03 +0200

    update templates state for delete action

--- src/app/messages/templates.page.state.ts
+++ src/app/messages/templates.page.state.ts
@@ -1,6 +1,9 @@
-export interface IUserAction {
-  readonly name: 'list'
-}
+import { EmailTemplate } from './email-template.model'
+import { TextTemplate } from './text-template.model'
+
+export type UserAction =
+  | { readonly name: 'list' }
+  | { readonly name: 'delete_template', template: EmailTemplate | TextTemplate }
 
 export interface IState<T> {
   readonly templates: ReadonlyArray<T>
