3f5dbe3bb929561e81a96458d58bef16991b03ca
developingstar <petr.nocolae812@gmail.com>
2018-08-20 10:49:42 +0200

    use User.API.IUser interface in the team members typings

--- src/app/settings/team-members/typings.d.ts
+++ src/app/settings/team-members/typings.d.ts
@@ -1,27 +1,15 @@
 declare namespace TeamSettings {
   type Role = 'admin' | 'sales_rep'
 
-  interface ISomething {
-    readonly avatar_url: string
-    readonly email: string
-    readonly first_name: string
-    readonly id: number
-    readonly last_name: string
-    readonly name: string
-    readonly phone_number: string
-    readonly password: string
-    readonly role: Role
-  }
-
   interface ITeamMembers {
     readonly data: {
-      readonly users: ReadonlyArray<ISomething>
+      readonly users: ReadonlyArray<Auth.API.IUser>
     }
   }
 
   interface ITeamMember {
     readonly data: {
-      readonly user: ISomething
+      readonly user: Auth.API.IUser
     }
   }
 
